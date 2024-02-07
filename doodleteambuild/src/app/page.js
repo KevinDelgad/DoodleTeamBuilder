03c0ed5481de03504ed954f0ed702f41771c66cc
KevinD <kevindel891@gmail.com>
2024-02-06 22:39:04 -0800

    create basic layout for website

--- doodleteambuild/src/app/page.js
+++ doodleteambuild/src/app/page.js
@@ -1,21 +1,35 @@
 import Image from "next/image";
+import { Fredoka } from "next/font/google";
+
+const fredoka = Fredoka({weight: '700', subsets: ['latin']})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full justify-center">
-        <h1 >Doodle World Teambuilder</h1>
-      </div>
-      <section className="flex flex-col">
-        <div>
-          <p>Team Defense</p>
-        </div>
-        <div>
-          <p>Team Coverage</p>
-        </div>
+    <main className="flex h-screen flex-col items-center justify-between bg-neutral-500">
+      <header className="flex w-full justify-center items-center bg-neutral-700 h-28">
+        <h1 className={`${fredoka.className} text-white lg:text-5xl sm:text-xl`}>Doodle World Teambuilder</h1>
+      </header>
+
+      <section className="flex flex-1 w-full justify-between">
+        <section className="flex basis-3/5">
+          <p>placeholder</p>
+        </section>
+
+        <section className="flex basis-2/5 flex-col">
+          <div>
+            <p>Team Defense</p>
+          </div>
+          <div>
+            <p>Team Coverage</p>
+          </div>
+        </section>
+
       </section>
-      <section className="flex">
-      </section>
+
     </main>
   );
 }
+
+/*
+
+*/
