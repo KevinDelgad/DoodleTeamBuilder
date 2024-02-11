80ac8a9fe97d08ec61abaac7a52cd3225f818497
KevinD <kevindel891@gmail.com>
2024-02-10 17:10:24 -0800

    Create initial Defense and Coverage Sections

--- doodleteambuild/src/app/page.js
+++ doodleteambuild/src/app/page.js
@@ -1,35 +1,77 @@
 import Image from "next/image";
 import { Fredoka } from "next/font/google";
 
-const fredoka = Fredoka({weight: '700', subsets: ['latin']})
+const fredoka = Fredoka({ weight: "700", subsets: ["latin"] });
+const doodleTypes = [
+  ["basic", "bg-basic"],
+  ["fire", "bg-fire"],
+  ["water", "bg-water"],
+  ["plant", "bg-plant"],
+  ["spark", "bg-spark"],
+  ["beast", "bg-beast"],
+  ["air", "bg-air"],
+  ["insect", "bg-insect"],
+  ["earth", "bg-earth"],
+  ["mind", "bg-mind"],
+  ["melee", "bg-melee"],
+  ["food", "bg-food"],
+  ["light", "bg-light"],
+  ["crystal", "bg-crystal"],
+  ["metal", "bg-metal"],
+  ["spirit", "bg-spirit"],
+  ["ice", "#bg-ice"],
+  ["dark", "bg-dark"],
+  ["poison", "bg-poison"],
+  ["mythic", "bg-mythic"],
+];
+
+const traitList = doodleTypes.map(doodle => <li className={`${fredoka.className} text-white badge-lg badge text-xl basis-1/5 outline-4 ${doodle[1]}`} >{doodle[0]}</li>);
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between bg-neutral-500">
-      <header className="flex w-full justify-center items-center bg-neutral-700 h-28">
-        <h1 className={`${fredoka.className} text-white lg:text-5xl sm:text-xl`}>Doodle World Teambuilder</h1>
+      <header className="flex w-full justify-center items-center bg-neutral-700 h-28 shrink-0">
+        <h1
+          className={`${fredoka.className} text-white lg:text-5xl sm:text-xl`}
+        >
+          Doodle World Teambuilder
+        </h1>
       </header>
 
-      <section className="flex flex-1 w-full justify-between">
+      <section className="flex w-full h-full justify-between">
         <section className="flex basis-3/5">
           <p>placeholder</p>
         </section>
 
-        <section className="flex basis-2/5 flex-col">
-          <div>
-            <p>Team Defense</p>
-          </div>
-          <div>
-            <p>Team Coverage</p>
-          </div>
+        <section className="flex basis-2/5 flex-col items-center">
+          <section className="flex flex-col basis-2/3 justify-around">
+            <div className="flex grow flex-col bg-stone-600 border-4 border-black rounded-2xl">
+              <p className={`${fredoka.className} text-white text-3xl flex justify-center`}>Team Defense</p>
+              <div className="flex grow">
+                <ul className="flex flex-wrap">{traitList}</ul>
+              </div>
+            </div>
+            <div className="flex grow flex-col bg-stone-600 border-4 border-black rounded-2xl">
+              <p className={`${fredoka.className} text-white text-3xl flex justify-center`}>Team Coverage</p>
+              <div className="flex grow">
+                <ul className="flex flex-wrap">{traitList}</ul>
+              </div>
+            </div>
+          </section>
+          <section className="flex-col justify-around flex shrink-0 basis-1/3">
+            <div>
+              <button>VIEW TYPE CHART</button>
+              <button>QUICK LOOKUP</button>
+            </div>
+
+            <div>
+              <button>Export a team</button>
+              <button>Import a team</button>
+            </div>
+
+          </section>
         </section>
-
       </section>
-
     </main>
   );
 }
-
-/*
-
-*/
