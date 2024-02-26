6573f9886aaac5badaa316f9d9b4d84320630d84
KevinD <kevindel891@gmail.com>
2024-02-26 15:06:05 -0800

    Create initial doodle Selection card

---
+++ doodleteambuild/src/app/components/doodleSelection.js
@@ -0,0 +1,11 @@
+function DoodleSelection({doodleName}){
+    return(
+        <input
+                          type="text"
+                          className="bg-neutral-600 text-2xl"
+                          placeholder= {doodleName}
+        />
+        )
+}
+
+export default DoodleSelection;
